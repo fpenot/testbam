e9bda3282a20a65e9af6a7867c20c539af33cd1a
fpenot <f.penot@gmail.com>
2015-02-25 06:46:45 +0100

    Avance sur module Company + Install Composer

--- src/eBAM/ObjectMgtBundle/Controller/CompanyController.php
+++ src/eBAM/ObjectMgtBundle/Controller/CompanyController.php
@@ -10,9 +10,20 @@ class CompanyController extends Controller
 {
     public function indexAction()
     {
-		return $this->render('eBAMObjectMgtBundle:Company:index.html.twig');
+		$listCompanies = array(
+			array (
+				'nom'	=> 'Société 1',
+				'id'	=> 1,
+				'accountAdmin' => 'Fabien PENOT'
+			)
+		);		
+		
+		return $this->render(
+			'eBAMObjectMgtBundle:Company:index.html.twig',
+			array('listCompanies' => $listCompanies)
+		);
     }
-	
+
     public function viewAction($id)
     {
 		return $this->render(
@@ -23,32 +34,30 @@ class CompanyController extends Controller
 	
 	public function addAction(Request $request)
 	{
-		// Si la requête est en POST, c'est que le visiteur a soumis le formulaire
-		if ($request->isMethod('POST')) {
-			// Ici, on s'occupera de la création et de la gestion du formulaire
-
-			$request->getSession()->getFlashBag()->add('notice', 'Compagnie bien enregistrée.');
-
-			// Puis on redirige vers la page de visualisation de cette compagnie
-			return $this->redirect($this->generateUrl('company_mgt_view', array('id' => 5)));
-		}
-
-		// Si on n'est pas en POST, alors on affiche le formulaire
-		return $this->render('eBAMObjectMgtBundle:Company:add.html.twig');	
+		$Company = array(
+			'nom'	=> 'Société 1',
+			'id'	=> 1,
+			'accountAdmin' => 'Fabien PENOT'
+		);		
+			
+		return $this->render(
+			'eBAMObjectMgtBundle:Company:add.html.twig',
+			array('company' => $Company)
+		);
 	}
 	
 	public function editAction($id, Request $request)
 	{
-		// Ici, on récupérera la comagnie correspondante à $id
-
-		// Même mécanisme que pour l'ajout
-		if ($request->isMethod('POST')) {
-		  $request->getSession()->getFlashBag()->add('notice', 'Compagnie bien modifiée.');
-
-		  return $this->redirect($this->generateUrl('company_mgt_view', array('id' => 5)));
-		}
-
-		return $this->render('eBAMObjectMgtBundle:Company:edit.html.twig');
+		$Company = array(
+			'nom'	=> 'Société 1',
+			'id'	=> $id,
+			'accountAdmin' => 'Fabien PENOT'
+		);		
+			
+		return $this->render(
+			'eBAMObjectMgtBundle:Company:edit.html.twig',
+			array('company' => $Company)
+		);
 	}
 
 	public function deleteAction($id)
